b6e32a3cbb5eeaf83839d752e226f9184e09b290
Yproshot <maiconpacheco@live.com>
2023-05-15 22:04:07 -0300

    academico

---
+++ components/Cabecalho.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Nav, Navbar, NavDropdown, NavLink, } from 'react-bootstrap'
+
+function Cabecalho() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="../anime">Acadêmico</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="../anime">Cursos</Nav.Link>
+            <Nav.Link href="../anime/generos">Diciplinas</Nav.Link>
+            <Nav.Link href="../anime/generos">Alunos</Nav.Link>
+            <Nav.Link href="../anime/generos">Professores</Nav.Link>
+            <Nav.Link href="../anime/generos">Turmas</Nav.Link>
+            <Nav.Link href="../anime/generos">Salas</Nav.Link>
+            <Nav.Link href="../anime/generos">Semestres</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+export default Cabecalho;
